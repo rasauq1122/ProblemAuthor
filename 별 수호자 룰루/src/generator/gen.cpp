98e97e21cd6e3ecc42181fa59edc4ad0a2b95ce2
rasauq1122 <rasauq1122@gmail.com>
2024-05-22 12:34:19 +0900

    Fix BOJ31873

--- 별 수호자 룰루/src/generator/gen.cpp
+++ 별 수호자 룰루/src/generator/gen.cpp
@@ -5,9 +5,19 @@ using namespace std;
 int main(int argc, char *argv[]) {
     registerGen(argc, argv, 1);
     int n_max = opt<int>("nMax");
+    int t_min = opt<int>("tMin");
+    bool odd = opt<bool>("odd");
     
-    int k = rnd.next(2, n_max/2);
-    int t = rnd.next(2, n_max/k);
+    int k = rnd.next(2, n_max/t_min);
+    
+    if (k == n_max/t_min) {
+        if (k%2 != odd) k--;
+    }
+    else {
+        if (k%2 != odd) k++;
+    }
+    
+    int t = rnd.next(t_min, n_max/k);
     
     cout << k*t << ' ' << k << endl;
 }
